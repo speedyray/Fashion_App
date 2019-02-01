88684ee4e2aa152196d4ebb7e0c6e7097aa2b6ed
Raynold <speedyray2ray@gmail.com>
2019-02-01 09:07:05 -0500

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,42 @@
+class ProductList extends React.Component {
+    render(){
+        return(<div className= 'ui stackable items' > 
+                <Product />
+              </div>
+          );
+        }
+
+    }
+
+   
+
+    class Product extends React.Component {
+        render(){
+            return(<div className= 'item' > 
+                     <div className= 'image' >
+                     <img src="images/products/image.png"/>
+                     </div> 
+                       <div className='middle aligned content'>
+                        <div className='description'>
+                           <a>Classy Middy Dress</a>
+                           <p>Amazing and adorable chic-sized dress, delivered in 3 days.</p>
+                        </div>
+                        <div className='extra'>
+                          <span>Submitted by:</span>
+                           <img
+                            className='ui avatar image'
+                            src='images/avatars/daniel.jpg'
+                             />
+                        </div>
+                     
+                     </div>
+                  </div>
+              );
+            }
+    
+        }
+        ReactDOM.render(
+         <ProductList />, 
+        document.getElementById('content')
+        );
+
